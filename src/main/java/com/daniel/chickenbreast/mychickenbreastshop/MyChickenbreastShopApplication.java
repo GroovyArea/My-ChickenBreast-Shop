873eb65117c52eb183f08fa86c414034ef87662f
NamYoung Kim <095201a@naver.com>
2022-07-29 09:30:41 +0900

    master - add : second version begun

---
+++ src/main/java/com/daniel/chickenbreast/mychickenbreastshop/MyChickenbreastShopApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.chickenbreast.mychickenbreastshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChickenbreastShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyChickenbreastShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/chickenbreast/mychickenbreastshop/MyChickenbreastShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.chickenbreast.mychickenbreastshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChickenbreastShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
