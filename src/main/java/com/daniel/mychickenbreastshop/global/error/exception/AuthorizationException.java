3495ae6a63c36ac386172061d1f298c692092bad
NamYoung Kim <095201a@naver.com>
2022-08-02 20:25:32 +0900

    feature/authorization - add : 애노테이션 기반 인가

---
+++ src/main/java/com/daniel/mychickenbreastshop/global/error/exception/AuthorizationException.java
@@ -0,0 +1,7 @@
+package com.daniel.mychickenbreastshop.global.error.exception;
+
+public class AuthorizationException extends RuntimeException {
+    public AuthorizationException(String message) {
+        super(message);
+    }
+}
